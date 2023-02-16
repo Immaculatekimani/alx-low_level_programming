e3d5957ea6dc6cfffbc0f60664d5baa9aa85f04b
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-02-16 09:08:57 +0300

    comment well

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * prints the size of various types on the computer it is compiled
- * and run on
- *
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 
 int main(void)
 {
